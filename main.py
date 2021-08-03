a3ba67b59ec696871bfe990f4a28114420c16b16
Shiki-Himura <athaus@hotmail.de>
2021-08-03 15:24:43 +0200

    Add Crossover function Add DNA list function Add mutate function Refactor DNA list function

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from random import choice
 
 target = 'Sein oder nicht sein'
 pool_lowercase = 'abcdefghijklmnopqrstuvwxyz '
-pool_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
+pool_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 parent_strings = []
 child_strings = []
 population_size = 2000
 mut_rate = 1
-fitness = 0
 
 
 # initialize randomly generated string and append it to parent-array
@@ -21,36 +20,53 @@ def initialize_rnd_str():
 
 # get 2000 randomly generated strings
 def create_parents():
-    for i in range(0, 2000):
+    for i in range(population_size):
         initialize_rnd_str()
 
 
 # get random number for the mutation
 # change char of string if mutation is invoked
-# def mutate():
+def mutate():
+    temp = []
+    for string in child_strings:
+        for i in string:
+            mutation = random.randint(0, 100)
+            if mutation < 1:
+                print(string)
+                 choice(pool_lowercase + pool_uppercase)
+        temp.append(string)
 
 
 # get list of strings * fitness
-def create_fitness_list():
-    global fitness
-    index = 0
+def create_dna_list():
+    temp = []
     for string in parent_strings:
-        for char in string:
-            if char == target[index]:
-                fitness += 1
-                index += 1
-                child_strings.append(string * fitness)
-            fitness = 0
-        index = 0
+        for i in range(len(string)):
+            if string[i] == target[i]:
+                temp.append(string)
+        temp.append(string)
+    return temp
 
 
-#def crossover_selection():
-# Implement mutation method. Implement selection method for child strings
+# Get strings and put 1/2 from each together
+def crossover_selection():
+    temp = create_dna_list()
+    for i in range(population_size):
+        rnd_string_one = choice(temp)
+        rnd_string_two = choice(temp)
+
+        string_one = rnd_string_one[:10]
+        string_one += rnd_string_two[10:]
+        string_two = rnd_string_one[10:]
+        string_two += rnd_string_two[:10]
+
+        child_strings.append(string_one)
+        child_strings.append(string_two)
 
 
 create_parents()
-print(parent_strings)
-create_fitness_list()
+crossover_selection()
+mutate()
 
 
 
